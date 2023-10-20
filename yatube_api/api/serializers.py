ed9a0989f3ff49a108f7049d52701387021589df
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-10-20 21:06:32 +0300

    (Cross-)posting checked.

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -9,37 +9,18 @@ class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)  # type: ignore[var-annotated]  # noqa: E501
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(  # type: ignore[var-annotated]
-        read_only=True, slug_field='username'
-    )
+        slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Comment
-
-
-# class CommentSerializer(serializers.ModelSerializer):
-#     author = serializers.SlugRelatedField(  # type: ignore[var-annotated]
-#         slug_field='username', read_only=True)
-
-#     class Meta:
-#         model = Comment
-#         fields = ('id', 'author', 'post', 'text', 'created')
-#         read_only_fields = ('post',)
-
-
-# class PostSerializer(serializers.ModelSerializer):
-#     author = serializers.SlugRelatedField(  # type: ignore[var-annotated]
-#         slug_field='username', read_only=True)
-
-#     class Meta:
-#         model = Post
-#         fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ('post',)
 
 
 class GroupSerializer(serializers.ModelSerializer):
